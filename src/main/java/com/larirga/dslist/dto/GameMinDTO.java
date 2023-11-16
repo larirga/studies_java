37174afa57f66446b0f57e30545bfa62a2d05c3a
larirga <larisssa_rga@hotmail.com>
2023-11-16 19:38:54 -0300

    add projection route {listId}/games

--- src/main/java/com/larirga/dslist/dto/GameMinDTO.java
+++ src/main/java/com/larirga/dslist/dto/GameMinDTO.java
@@ -2,6 +2,7 @@ package com.larirga.dslist.dto;
 
 
 import com.larirga.dslist.entities.Game;
+import com.larirga.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
   private Long id;
@@ -19,6 +20,14 @@ public class GameMinDTO {
     year = entity.getYear();
     imgUrl = entity.getImgUrl();
     shortDescription = entity.getShortDescription();
+  }
+
+  public GameMinDTO(GameMinProjection projection) {
+    id = projection.getId();
+    title = projection.getTitle();
+    year = projection.getYear();
+    imgUrl = projection.getImgUrl();
+    shortDescription = projection.getShortDescription();
   }
 
   public Long getId() {
--- src/main/java/com/larirga/dslist/services/GameService.java
+++ src/main/java/com/larirga/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.larirga.dslist.services;
 import com.larirga.dslist.dto.GameDTO;
 import com.larirga.dslist.dto.GameMinDTO;
 import com.larirga.dslist.entities.Game;
+import com.larirga.dslist.projections.GameMinProjection;
 import com.larirga.dslist.repositories.GameRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,13 @@ public class GameService {
   @Transactional(readOnly = true)
   public List<GameMinDTO> findAll() {
     List<Game> result = gameRepository.findAll();
+    List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+    return dto;
+  }
+
+  @Transactional(readOnly = true)
+  public List<GameMinDTO> findByList(Long listId) {
+    List<GameMinProjection> result = gameRepository.searchByList(listId);
     List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
     return dto;
   }
