b62695c707d7e910ea50cace2f94c0cde417dee9
larirga <larisssa_rga@hotmail.com>
2023-11-16 18:51:37 -0300

    add route GET game by id

--- src/main/java/com/larirga/dslist/controllers/GameController.java
+++ src/main/java/com/larirga/dslist/controllers/GameController.java
@@ -1,11 +1,12 @@
 package com.larirga.dslist.controllers;
 
+import com.larirga.dslist.dto.GameDTO;
 import com.larirga.dslist.dto.GameMinDTO;
-import com.larirga.dslist.entities.Game;
 import com.larirga.dslist.services.GameService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class GameController {
   @Autowired
   private GameService gameService;
+
+  @GetMapping(value = "/{id}")
+  public GameDTO findById(@PathVariable Long id) {
+    GameDTO result = gameService.findById(id);
+    return result;
+  }
 
   @GetMapping
   public List<GameMinDTO> findAll() {
